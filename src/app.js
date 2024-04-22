899022e1caeeffb25e10b3ebb8b59de802e5fb42
Navin Chandrakar <88479575+navinfc@users.noreply.github.com>
2024-04-23 00:36:53 +0530

    util code disscussion

--- src/app.js
+++ src/app.js
@@ -11,5 +11,11 @@ app.use(cors({
 
 app.use(express.json({limit: "16kb"}));
 
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
 export { app };
 
